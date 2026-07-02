059a436d1362dbc7c0329eeb9a239e3116e8736e
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-02 10:19:00 +0400

    Added Task 1 countdown solution

--- work sheet 2/Program.cs
+++ work sheet 2/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Task 1
+        Console.WriteLine("Enter a number:");
+        int number =  int.Parse(Console.ReadLine());
+
+        for ( int i = number; i >= 1; i--)
+        {
+          Console.WriteLine(i);  
+        }
+
+        Console.WriteLine("Liftoff");
     }
 }
