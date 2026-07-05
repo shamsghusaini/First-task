8cfdf53969660b802ce9eb312e11c20f1cbb10ac
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-05 13:45:07 +0400

    Complete task 1 : Absolute Differemce

---
+++ work sheet 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace work_sheet_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        // Task 1:
+        Console.WriteLine("Enter the First Number:");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the Second Number:");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+        
+        double positiveDifference = Math.Abs(secondNumber - firstNumber);
+        Console.WriteLine("The positive difference is: " + positiveDifference);
+        
+        
+        
+        
+    }
+}
