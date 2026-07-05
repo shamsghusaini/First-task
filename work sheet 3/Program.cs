d12e654a50a7f3e8d04e9de9d7e450b26b776129
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-05 13:55:13 +0400

    Complite Task 2 : Power and Root  Explorer using Math.pow and math.sqrt

--- work sheet 3/Program.cs
+++ work sheet 3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
         
         // Task 1:
         Console.WriteLine("Enter the First Number:");
@@ -14,6 +14,17 @@ class Program
         
         double positiveDifference = Math.Abs(secondNumber - firstNumber);
         Console.WriteLine("The positive difference is: " + positiveDifference);
+        
+        
+        // Task 2: 
+        Console.WriteLine(" Enter Number");
+        int number = int.Parse(Console.ReadLine());
+        
+        double square = Math.Pow(number, 2);
+        double squareRoot = Math.Sqrt(number);
+        
+        Console.WriteLine("The square root of " + number + " is " + squareRoot);
+        Console.WriteLine("The square of " + number + " is " + squareRoot);
         
         
         
