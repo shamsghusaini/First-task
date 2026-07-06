0dc8c59e0ecdf06236ea559a2d5b2bb30ef7a5bf
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-06 23:26:34 +0400

    Complete Task 2: Square Number Function

--- work sheet 4/Program.cs
+++ work sheet 4/Program.cs
@@ -2,6 +2,7 @@
 
 class Program
 {
+    /*
     static void PrintWelcome(string name)
     {
         Console.WriteLine("Welcome " + name);
@@ -13,5 +14,18 @@ class Program
         Console.WriteLine("Enter your Name: ");
         string name = Console.ReadLine();
         PrintWelcome(name);
+    }
+*/
+    static int Square(int number)
+    {
+        return number * number;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int result = Square(number);
+        Console.WriteLine("The square is " + result);
     }
 }
