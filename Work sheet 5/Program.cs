9dcf3ea8a4c57d1dca0fc30bed4ee1170cf6af37
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-08 14:02:10 +0400

    Complete Task 2 : Dynamic To do List

--- Work sheet 5/Program.cs
+++ Work sheet 5/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        
        // Task 1 :
        int[] grades = new int[5];
 
@@ -22,6 +23,21 @@ class Program
        }
        
        
+       // Task 2: 
+       List<string> tasks = new List<string>();
        
+       for (int i = 0; i < 5; i++)
+       {
+           Console.WriteLine($"Enter Task{i + 1}: ");
+           string task = Console.ReadLine();
+           tasks.Add(task);
+       }
+       
+       Console.WriteLine();
+       Console.WriteLine("To do List");
+           foreach (string task in tasks)
+               {
+               Console.WriteLine( "- " + task);
+               }
     }
 }
