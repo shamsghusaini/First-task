4fdab2e4798d6b253f60a0512dcf97ae380eb319
Shams Al Ghusaini <shamsalghusainii@gmail.com>
2026-07-08 13:51:55 +0400

    Complete Task 1 : Fixed Grades Array

---
+++ Work sheet 5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Work_sheet_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Task 1 :
+       int[] grades = new int[5];
+
+       // Input grades
+       for (int i = 0; i < grades.Length; i++)
+       {
+           Console.Write($"Enter grade {i + 1}: ");
+           grades[i] = Convert.ToInt32(Console.ReadLine());
+       }
+
+       // Print grades
+       Console.WriteLine("\nStudent Grades:");
+       foreach (int grade in grades)
+       {
+           Console.WriteLine(grade);
+       }
+       
+       
+       
+    }
+}
